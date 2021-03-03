2a704080763fadd56c37fcddc4d267924e1cbbe2
Staffan Småros <staffan.smaros@gmail.com>
2021-03-03 22:26:22 +0200

    Improved tab styling and functionality

--- src/index.js
+++ src/index.js
@@ -9,22 +9,36 @@ const main = document.querySelector('main');
 
 const navButtons = document.querySelectorAll('.nav-btn')
 navButtons.forEach(button => button.addEventListener('click', function(e) {
+    e.target.classList.toggle('active');
     main.innerHTML = '';
     switch(e.target.id) {
         case 'home-btn':
+            activateTab('home-btn')
             loadHome().forEach(element => {
                 main.appendChild(element);
             });
             break;
         case 'menu-btn':
+            activateTab('menu-btn')
             loadMenu().forEach(element => {
                 main.appendChild(element);
             });
             break;
         case 'contact-btn':
+            activateTab('contact-btn')
             loadContact().forEach(element => {
                 main.appendChild(element);
             });
             break;
     };
 }))
+
+function activateTab(id) {
+    navButtons.forEach(button => {
+        if(button.id === id && !button.classList.contains('active')) {
+            button.classList.add('active');
+        } else if (button.id != id && button.classList.contains('active')) {
+            button.classList.remove('active');
+        };
+    });
+};
--- src/modules/loadPage.js
+++ src/modules/loadPage.js
@@ -5,6 +5,8 @@ function loadPage() {
     const contentContainer = document.getElementById('content');
     
     contentContainer.appendChild(loadHeader());
+    //Activate first tab
+    document.querySelector('#home-btn').classList.add('active');
     
     contentContainer.appendChild(loadMain());
 };
